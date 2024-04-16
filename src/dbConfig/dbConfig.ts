125c3093295dc43c063c81bf88494f4928ebc512
sinha97 <vk9633698@gmail.com>
2024-04-16 10:12:52 +0530

    dg config added

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,22 @@
+import mongoose, { connection } from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("MongoDB connected");
+    });
+
+    connection.on("error", (err) => {
+      console.log(
+        "MongoDB connection error,please make sure DB is up and running :" + err
+      );
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something went wrong in connecting to DB");
+    console.log(error);
+  }
+}
