bb2daee9bd3ce7b08a20a5ca2f0d664720ac21f7
sinha97 <vk9633698@gmail.com>
2024-05-16 08:42:20 +0530

    add functionality of signup

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+'use client'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
-export default page
+const SingupPage = () => {
+  const router = useRouter();
+  const [user, setuser] = useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const onSignup = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.post("/api/users/signup", user);
+      console.log("signup success", response.data);
+      router.push("/login");
+    } catch (error: any) {
+      console.log("Signup failed");
+      toast.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    if (
+      user.email.length > 0 &&
+      user.password.length > 0 &&
+      user.username.length > 0
+    ) {
+      setButtonDisabled(false);
+    } else {
+      setButtonDisabled(true);
+    }
+  }, [user]);
+
+  return <div>SingupPage</div>;
+};
+
+export default SingupPage;
